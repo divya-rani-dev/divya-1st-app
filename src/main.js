0463a7b85f3229a5dbd3b6da19f8a532d72702ed
divya-rani-dev <61187283+divya-rani-dev@users.noreply.github.com>
2020-07-19 17:50:15 +0530

    Added Bootstrap
    Fixed Bootstrap Issue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 new Vue({
   vuetify,
   render: h => h(App)
