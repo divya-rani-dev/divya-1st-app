c0ba9c50e1ed4e493cd0a03dde09c8fc255778a7
divya-rani-dev <61187283+divya-rani-dev@users.noreply.github.com>
2020-09-01 10:49:45 +0530

    Commit 01/09/2020

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,4 @@
 <template>
-  <div class="text-center">
-    <br>
-    <br>
-    <v-tooltip right>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >Left</v-btn>
-      </template>
-      <span>Left tooltip</span>
-    </v-tooltip>
-    <div class="alert alert-primary" role="alert" style="padding: 30%">Simple Alert Box</div>
-  </div>
+<p>divya</p>
+
 </template>
